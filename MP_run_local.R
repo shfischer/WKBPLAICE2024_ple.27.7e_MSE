c43bcd55fa193a2c358350c484f06c79a1d6a281
shfischer <32540427+shfischer@users.noreply.github.com>
2026-02-10 16:28:16 +0000

    update MP_run_local.R for R_h_lower

--- MP_run_local.R
+++ MP_run_local.R
@@ -56,7 +56,7 @@ if (FALSE) {
 OMs <- c("refset", 
          "baseline", "Catch_no_disc", "Catch_no_surv", "migr_none", 
          "M_low", "M_high", "M_Gislason", 
-         "R_no_AC", "R_higher", "R_lower", 
+         "R_no_AC", "R_higher", "R_lower", "R_h_lower",
          "R_failure", "overcatch", "undercatch", "Idx_higher")
 
 
@@ -85,69 +85,6 @@ OMs <- c("refset",
   
 }
 
-### ------------------------------------------------------------------------ ###
-### baseline - hr - x & w & v & n0 ####
-### ------------------------------------------------------------------------ ###
-args_local <- c("n_blocks=1", "n_workers=0", 
-                "scenario='mult_comp_b_mult'", "MP='hr'",
-                "n_yrs=20", "check_file=FALSE",
-                "ga_search=TRUE", "OM='baseline'", "save_MP=TRUE", 
-                "popSize=1", "maxiter=1",
-                "add_suggestions=FALSE", "collate=FALSE",
-                "idxB_lag=1", "idxB_range_3=1", "exp_b=1", 
-                "comp_b_multiplier=0.66", "interval=1", 
-                "multiplier=0.64",
-                "upper_constraint=1.2", "lower_constraint=0.7")
-source("MP_run.R")
-
-rm(args_local)
-args_local <- "n_blocks=1"
-idxB_range_3 <- 1
-interval <- 2
-comp_b_multiplier <- 0.68
-multiplier <- 0.61
-source("MP_run.R")
-
-idxB_range_3 <- 2
-interval <- 1
-comp_b_multiplier <- 0.92
-multiplier <- 0.59
-source("MP_run.R")
-
-idxB_range_3 <- 2
-interval <- 2
-comp_b_multiplier <- 0.87
-multiplier <- 0.58
-source("MP_run.R")
-
-
-
-### ------------------------------------------------------------------------ ###
-### reference set - hr optimised multiplier ####
-### ------------------------------------------------------------------------ ###
-res <- readRDS("output/ple.27.7e/refset/1000_20/multiplier/hr/multiplier-upper_constraint1.2-lower_constraint0.7--obj_ICES_res_11-20.rds")
-res@solution[, "multiplier"]
-### 0.6
-### but slightly above 5% risk -> use 0.59
-args_local <- c("n_blocks=10", "n_workers=10", "mp_parallel=TRUE",
-                "scenario='multiplier'", "MP='hr'",
-                "n_yrs=20", "check_file=FALSE",
-                "ga_search=TRUE", "OM='refset'", "MP='hr'", "save_MP=TRUE", 
-                "popSize=1", "maxiter=1",
-                "add_suggestions=FALSE", "collate=FALSE",
-                "idxB_lag=1", "idxB_range_3=1", "exp_b=1", 
-                "comp_b_multiplier=1.4", "interval=1", 
-                "multiplier=0.59",
-                "upper_constraint=1.2", "lower_constraint=0.7")
-source("MP_run.R")
-
-### with Q1SWBeam index
-rm(args_local)
-args_local <- "a=1"
-biomass_index <- "Q1SWBeam"
-scenario <- "multiplier_Q1SWBeam"
-multiplier <- 0.75
-source("MP_run.R")
 
 ### ------------------------------------------------------------------------ ###
 ### SAM - all OMs ####
@@ -162,7 +99,7 @@ source("MP_run.R")
 stopCluster(cl); rm(cl); gc()
 rm(args_local)
 ### other OMs
-alt_OMs <- c("Catch_no_disc", "Catch_no_surv", "migr_none", "M_low", "M_high", "M_Gislason", "R_no_AC", "R_higher", "R_lower", "R_failure", "overcatch", "undercatch", "Idx_higher")
+alt_OMs <- c("Catch_no_disc", "Catch_no_surv", "migr_none", "M_low", "M_high", "M_Gislason", "R_no_AC", "R_higher", "R_lower", "R_h_lower", "R_failure", "overcatch", "undercatch", "Idx_higher")
 for (OM in alt_OMs) {
   print(paste0("OM=", OM))
   args_local <- c(paste0("OM='", OM, "'"))
@@ -190,37 +127,17 @@ for (OM in alt_OMs) {
 }
 
 ### ------------------------------------------------------------------------ ###
-### baseline - hr - optimise multiplier - Q1SWBeam ####
-### ------------------------------------------------------------------------ ###
-args_local <- c("n_blocks=1", "n_workers=15", "ga_parallel=TRUE",
-                "scenario='multiplier_Q1SWBeam'", "MP='hr'",
-                "biomass_index='Q1SWBeam'",
-                "n_yrs=20", "check_file=FALSE",
-                "ga_search=TRUE", "OM='baseline'", "MP='hr'", "save_MP=FALSE", 
-                "popSize=1", "maxiter=1",
-                "add_suggestions=FALSE", "collate=FALSE",
-                "idxB_lag=1", "idxB_range_3=1", "exp_b=1", 
-                "comp_b_multiplier=1.4", "interval=1", 
-                "multiplier=seq(0,2,0.01)",
-                "upper_constraint=1.2", "lower_constraint=0.7")
-source("MP_run.R")
-
-### ------------------------------------------------------------------------ ###
 ### refset x & w -> all OMs ####
 ### ------------------------------------------------------------------------ ###
 
 ### get optimised solutions
-df_x <- readRDS("output/refset_x_runs_opt.rds")
-df_x_w <- readRDS("output/refset_x_w_grid_opt.rds")
-df_x_w <- bind_rows(
-  df_x %>% mutate(optimum = "global"), 
-  df_x_w)
+df_optima <- readRDS("output/paper/refset_x_w_grid_opt.rds")
 
 OMs <- c("refset", "baseline", "Catch_no_disc", "Catch_no_surv", "migr_none", 
-         "M_low", "M_high", "M_Gislason", "R_no_AC", "R_higher", "R_lower", 
-         "R_failure", "overcatch", "undercatch", "Idx_higher")
+         "M_low", "M_high", "M_Gislason", "R_no_AC", "R_higher", "R_lower",
+         "R_h_lower", "R_failure", "overcatch", "undercatch", "Idx_higher")
 
-. <- foreach(x = split(df_x_w, f = seq(nrow(df_x_w)))) %:% 
+. <- foreach(x = split(df_optima, f = seq(nrow(df_optima)))) %:% 
   foreach(OM = OMs) %do% {
     #browser()
     print(x)
@@ -259,16 +176,12 @@ OMs <- c("refset", "baseline", "Catch_no_disc", "Catch_no_surv", "migr_none",
 }
 
 ### ------------------------------------------------------------------------ ###
-### refset - MP5 100 years ####
+### refset - CHR2 100 years ####
 ### ------------------------------------------------------------------------ ###
 
 ### get optimised solutions
-df_x <- readRDS("output/refset_x_runs_opt.rds")
-df_x_w <- readRDS("output/refset_x_w_grid_opt.rds")
-df_x_w <- bind_rows(
-  df_x %>% mutate(optimum = "global"), 
-  df_x_w)
-pars <- df_x_w[df_x_w$MP == 5, ]
+df_optima <- readRDS("output/paper/refset_x_w_grid_opt.rds")
+pars <- df_optima[df_optima$MP == 2, ]
 
 n_blocks <- 7 
 n_workers <- 7 
@@ -301,17 +214,13 @@ source("MP_run.R")
 ### sensitivity - index uncertainty ####
 ### ------------------------------------------------------------------------ ###
 
-df_x <- readRDS("output/refset_x_runs_opt.rds")
-df_x_w <- readRDS("output/refset_x_w_grid_opt.rds")
-df_x_w <- bind_rows(
-  df_x %>% mutate(optimum = "global"), 
-  df_x_w)
+df_optima <- readRDS("output/paper/refset_x_w_grid_opt.rds")
 
 OMs <- c("refset", "baseline", "Catch_no_disc", "Catch_no_surv", "migr_none", 
-         "M_low", "M_high", "M_Gislason", "R_no_AC", "R_higher", "R_lower", 
-         "R_failure", "overcatch", "undercatch", "Idx_higher")
+         "M_low", "M_high", "M_Gislason", "R_no_AC", "R_higher", "R_h_lower",
+         "R_lower", "R_failure", "overcatch", "undercatch", "Idx_higher")
 
-. <- foreach(x = split(df_x_w, f = seq(nrow(df_x_w)))) %:% 
+. <- foreach(x = split(df_optima, f = seq(nrow(df_optima)))) %:% 
   foreach(OM = OMs[1]) %do% {
     #browser()
     print(x)
